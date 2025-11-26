bd5b90e066859729e0b165448e815313aee26c11
Sanda2 <santiagoaristimuno2005@gmail.com>
2025-11-26 10:05:25 -0400

    merge con mi origin

--- src/main/java/com/goats/os_proyect2/Os_Proyect2.java
+++ src/main/java/com/goats/os_proyect2/Os_Proyect2.java
@@ -50,6 +50,7 @@ public class Os_Proyect2 {
                 "Estado: Listo para operaciones",
                 "Bienvenido",
                 JOptionPane.INFORMATION_MESSAGE);
+                //exx
         });
     }
 }
